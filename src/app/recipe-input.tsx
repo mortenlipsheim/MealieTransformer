79accf056bad60d2c71a9706a988a30cddc6fd59
Morten Gausland <morten@gmail.com>
2025-08-10 16:51:07 +0000

    OK. I used my Cloudflare Zero Trust tunnel to access my local instance.

--- src/app/recipe-input.tsx
+++ src/app/recipe-input.tsx
@@ -50,7 +50,7 @@ export default function RecipeInput() {
       }
 
       try {
-        const stream = await navigator.mediaDevices.getUserMedia({video: true});
+        const stream = await navigator.mediaDevices.getUserMedia({video: { facingMode: 'environment' }});
         setHasCameraPermission(true);
 
         if (videoRef.current) {
